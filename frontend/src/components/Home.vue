dab81c5e6f761ab893f7997d406f07146dc52e39
swpi <coolonce@yandex.ru>
2018-07-06 02:20:50 +0200

    first

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <p>Home page</p>
+    <p>{{randomNumber}}</p>
+    <button @click="getRandom">Get Random</button>
+  </div>
+</template>
+<script>
+import axios from 'axios'
+export default{
+
+  data () {
+    return {
+      randomNumber: 0
+    }
+  },
+  methods: {
+    getRandom () {
+      const path = `http://localhost:5000/api/random`
+      axios.get(path)
+        .then(response => {
+          this.randomNumber = response.data.randomNumber
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    }
+  }
+}
+</script>
